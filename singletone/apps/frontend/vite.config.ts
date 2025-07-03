c1fc96fc9ffc4fb7697ff44c7ced1de541d3980f
RoudiOlding <roudiolding040504@gmail.com>
2025-07-03 03:42:10 -0500

    roudi: estructura mayor del proyecto y corrección parcial de ubicaciones.

--- singletone/apps/frontend/vite.config.ts
+++ singletone/apps/frontend/vite.config.ts
@@ -6,7 +6,7 @@ import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default defineConfig(() => ({
   root: __dirname,
-  cacheDir: '../node_modules/.vite/frontend',
+  cacheDir: '../../node_modules/.vite/apps/frontend',
   server: {
     port: 4200,
     host: 'localhost',
@@ -21,7 +21,7 @@ export default defineConfig(() => ({
   //  plugins: [ nxViteTsPaths() ],
   // },
   build: {
-    outDir: '../dist/frontend',
+    outDir: '../../dist/apps/frontend',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
